ba2e756c2afe878fc7502ea608feab6a349074a6
anjuli <anjuliw@gmail.com>
2016-12-04 00:24:05 -0800

    searchBar event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	render() {
-		return <input />;
+		// class components must have render method with some JSX
+		// event handler with arrow function
+		return <input onChange={event => console.log(event.target.value)} />;
 	}
 }
-
-//new SearchBar
 
 export default SearchBar;
