684083519a1a8cd9d3402c977b46d1ff968eb606
anjuli <anjuliw@gmail.com>
2016-12-03 21:40:38 -0800

    search_bar as simple text input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 // create new component. shoud produce some HTML.
 const App = () => {
-	return <div>hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+		);
 }
 
 // take this component's generated HTML and insert it in the DOM
