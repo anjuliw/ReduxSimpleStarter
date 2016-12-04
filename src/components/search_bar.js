6a843253ebe61353141ea55ab5d0b1fc6de71481
anjuli <anjuliw@gmail.com>
2016-12-04 00:49:43 -0800

    searchBar with state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		// initialize state, only time anything is assigned to this.state
+		this.state = {term: '' };
+	}
+
 	render() {
 		// class components must have render method with some JSX
 		// event handler with arrow function
-		return <input onChange={event => console.log(event.target.value)} />;
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value })} />
+			</div>
+			);
 	}
 }
 
